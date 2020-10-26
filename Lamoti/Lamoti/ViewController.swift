4da3b3788d19c929f9018082de41409a0637c58c
JeongSuhong <ghjkdfe@gmail.com>
2020-10-26 10:26:51 +0900

    1. Add Pod RemoteConfig 2. Add Splash

--- Lamoti/Lamoti/ViewController.swift
+++ Lamoti/Lamoti/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import SnapKit
+import Firebase
 
 class ViewController: UIViewController {
+
+    let logo = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        logo.image = UIImage(named: "app_logo")
+        
+        self.view.addSubview(logo)
+        logo.snp.makeConstraints { (make) in
+        make.center.equalTo(self.view)
+        }
+        
     }
-
-
 }
 
