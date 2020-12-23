2fa38ab4cfc75565690f2498438f75e86d97890d
JeongSuhong <ghjkdfe@gmail.com>
2020-12-23 20:19:11 +0900

    로그인 구조 변경 및 오류 팝업 수정

---
+++ Lamoti/Lamoti/Login/View/LoginViewController.swift
@@ -0,0 +1,76 @@
+//
+//  LoginViewController.swift
+//  Lamoti
+//
+//  Created by Suhong Jeong on 2020/10/29.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    private var vm : LoginViewModel?
+
+    
+    
+    override func viewDidLoad() {
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.backgroundColor = .clear
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        vm = LoginViewModel()
+        vm?.delegate = self
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        vm?.delegate = nil
+        vm = nil
+    }
+
+    @IBAction func loginEvent(_ sender: UIButton) {
+        vm?.startLogin(email: emailText.text, password: passwordText.text)
+    }
+    
+    @IBAction func presentSignIn(_ sender: UIButton) {
+        let view = self.storyboard?.instantiateViewController(identifier: "SignInViewController") as! SignInViewController
+        self.navigationController?.pushViewController(view, animated: true)
+    }
+}
+
+
+
+extension LoginViewController : LoginProtocol {
+    
+    func successEvent() {
+    }
+    
+    func failedEvent(_ error: NSError) {
+        
+        var message : String?
+        
+        switch AuthErrorCode(rawValue: error.code) {
+        case .networkError:
+            message = "네트워크 문제로 서비스 접속에 실패했습니다.\n네트워크 연결을 확인한 후 다시 이용해 주세요."
+        case .invalidEmail:
+            message = "이메일 형식에 맞지 않는 메일주소입니다.\n다시 입력해주시기 바랍니다."
+        case .wrongPassword:
+            message = "등록되지 않은 아이디이거나 아이디 또는 비밀번호를 잘못 입력했습니다"
+        default:
+            message = "일시적인 오류로 로그인을 할 수 없습니다.\n잠시 후 다시 이용해 주시기 바랍니다."
+        }
+        
+        let alert = UIAlertController(title: "로그인 실패", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+}
--- Lamoti/Lamoti/Login/ViewModel/LoginViewModel.swift
+++ Lamoti/Lamoti/Login/ViewModel/LoginViewModel.swift
@@ -8,17 +8,54 @@
 import Firebase
 
 protocol LoginProtocol {
-    func LoginSuccess()
-    func LoginFailed()
+    func successEvent()
+    func failedEvent(_ error: NSError)
 }
 
 class LoginViewModel {
     
+    private var listenerHandle : AuthStateDidChangeListenerHandle?
+    
+    var delegate : LoginProtocol?
+    
+    func startLogin(email: String?, password: String?) {
+        self.listenerHandle = Auth.auth().addStateDidChangeListener(loginChangeState)
+        
+        Auth.auth().signIn(withEmail: email!, password: password!) { (result, error) in
+            if(error != nil) {
+                self.failedLogin(error! as NSError)
+            }
+        }
+    }
+    
+    func successLogin() {
+        let uid = Auth.auth().currentUser?.uid
+        Messaging.messaging().token { (token, error) in
+            
+            if error != nil {
+                
+            let data = NSDictionary(dictionary: ["pushToken":token!])
+            Database.database().reference().child("users").child(uid!).updateChildValues(data as! [AnyHashable : Any])
+                
+                Auth.auth().removeStateDidChangeListener(self.listenerHandle!)
+                self.delegate?.successEvent()
+            } else {
+                self.failedLogin(error! as NSError)
+            }
+        }
+    }
+    
+    func failedLogin(_ error: NSError) {
+        
+        Auth.auth().removeStateDidChangeListener(self.listenerHandle!)
+        self.delegate?.failedEvent(error)
+    }
     
     
-    
-    func startLogin(email: String, password: String) {
-        Auth.auth().addStateDidChangeListener(<#T##listener: (Auth, User?) -> Void##(Auth, User?) -> Void#>)
+    func loginChangeState(auth: Auth, user: User?) {
+        if(user != nil) {
+            successLogin()
+        }
     }
     
 }
